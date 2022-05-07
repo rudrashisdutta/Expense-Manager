1cb30b78095be03e3f10e8cebf71f433e3c452d7
rudrashisdutta <rudra2001official@gmail.com>
2022-05-08 01:04:28 +0530

    Some styling was added to expenses component.
    08-05-2022 01-04a

--- src/App.js
+++ src/App.js
@@ -3,9 +3,26 @@ import classes from "./App.module.css";
 import Expenses from "./components/Expenses/Expenses";
 
 function App() {
+  const expenses = [
+    {
+      title: "Gift",
+      date: new Date("2020-08-25"),
+      amount: 300.0,
+    },
+    {
+      title: "Mobile Phone",
+      date: new Date("2020-04-30"),
+      amount: 400.0,
+    },
+    {
+      title: "Laptop",
+      date: new Date("2020-08-28"),
+      amount: 1350.6,
+    },
+  ];
   return (
     <div>
-      <Expenses />
+      <Expenses expenses={expenses} />
     </div>
   );
 }
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,26 +2,9 @@ import classes from "./Expenses.module.css";
 import ExpenseItem from "./ExpenseItem/ExpenseItem";
 import React from "react";
 
-export default function Expenses(props) {
-  const expenses = [
-    {
-      title: "Gift",
-      date: new Date("2020-08-25"),
-      amount: 300.0,
-    },
-    {
-      title: "Mobile Phone",
-      date: new Date("2020-04-30"),
-      amount: 400.0,
-    },
-    {
-      title: "Laptop",
-      date: new Date("2020-08-28"),
-      amount: 1350.6,
-    },
-  ];
+export default function Expenses({ expenses }) {
   return (
-    <div>
+    <div className={classes.expenses}>
       {expenses.map((expense) => {
         return (
           <ExpenseItem
