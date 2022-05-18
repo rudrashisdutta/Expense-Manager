9773d90e3bacb5cc59200b1bf6444494312a6f07
rudrashisdutta <rudra2001official@gmail.com>
2022-05-19 03:20:11 +0530

    Added states to New ExpenseForm component to keep track of changes in the form inputs.
    19-05-2022 03-20a

--- src/components/new_expense/expense_form/ExpenseForm.js
+++ src/components/new_expense/expense_form/ExpenseForm.js
@@ -1,21 +1,45 @@
 import classes from "./ExpenseForm.module.css";
-import React from "react";
+import React, { useState } from "react";
 
 export default function ExpenseForm({ setShow }) {
+  const [title, setTitle] = useState("");
+  const [amount, setAmount] = useState("");
+  const [date, setDate] = useState("");
+
+  const onTitleChange = (e) => {
+    setTitle(e.target.value);
+  };
+  const onAmountChange = (e) => {
+    setAmount(Number(e.target.value));
+  };
+  const onDateChange = (e) => {
+    setDate(new Date(e.target.value));
+  };
+
   return (
     <form>
       <div className={classes.new_expense__controls}>
         <div className={classes.new_expense__control}>
           <label>Title</label>
-          <input type={"text"} />
+          <input type={"text"} onChange={onTitleChange} />
         </div>
         <div className={classes.new_expense__control}>
           <label>Amount</label>
-          <input type={"number"} min={"0.01"} step={"0.01"} />
+          <input
+            type={"number"}
+            min={"0.01"}
+            step={"0.01"}
+            onChange={onAmountChange}
+          />
         </div>
         <div className={classes.new_expense__control}>
           <label>Date</label>
-          <input type={"date"} min={"2019-01-01"} max={"2022-12-31"} />
+          <input
+            type={"date"}
+            min={"2019-01-01"}
+            max={"2022-12-31"}
+            onChange={onDateChange}
+          />
         </div>
       </div>
       <div className={classes.new_expense__actions}>
