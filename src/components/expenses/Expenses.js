3e92caa52865eca0d8b5d1cdefb88067912750d2
rudrashisdutta <rudra2001official@gmail.com>
2022-05-27 15:10:15 +0530

    Expenses will soon be able to get filtered by year
    The Expense Filter component will help filter the existing expenses by year.
    
    27-05-2022 03-10p

--- src/components/expenses/Expenses.js
+++ src/components/expenses/Expenses.js
@@ -1,11 +1,18 @@
 import classes from "./Expenses.module.css";
 import ExpenseItem from "./expense_item/ExpenseItem";
-import React from "react";
+import React, { useState } from "react";
 import Card from "../card/Card";
+import ExpensesFilter from "./expenses_filter/ExpensesFilter";
 
 export default function Expenses({ expenses }) {
+  const [filter, setFilter] = useState(2022);
+  const addExpensesFilter = (filter) => {
+    console.log(filter);
+    setFilter(filter);
+  };
   return (
     <Card className={classes.expenses}>
+      <ExpensesFilter onAddExpensesFilter={addExpensesFilter} />
       {expenses.map((expense) => {
         return (
           <ExpenseItem
---
+++ src/components/expenses/expenses_filter/ExpensesFilter.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import classes from "./ExpensesFilter.module.css";
+
+const ExpensesFilter = ({ onAddExpensesFilter }) => {
+  const onSelectedExpenseFilterChange = (e) => {
+    onAddExpensesFilter(Number(e.target.value));
+  };
+  return (
+    <div className={classes.expenses_filter}>
+      <div className={classes.expenses_filter__control}>
+        <label>Filter by year</label>
+        <select onChange={onSelectedExpenseFilterChange}>
+          <option value="2022">2022</option>
+          <option value="2021">2021</option>
+          <option value="2020">2020</option>
+          <option value="2019">2019</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default ExpensesFilter;
