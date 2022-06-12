39cb4e322dd1192baa5637c231f324b89e655820
rudrashisdutta <rudra2001official@gmail.com>
2022-06-12 13:42:44 +0530

    Minor changes to the UI.
    12-06-2022 01-42p

--- src/components/chart/chart_bar/ChartBar.js
+++ src/components/chart/chart_bar/ChartBar.js
@@ -7,15 +7,17 @@ function ChartBar({ value, maxValue, label }) {
     barFillHeight = Math.round((value / maxValue) * 100) + "%";
   }
   return (
-    <div className={classes.chart_bar}>
-      <div className={classes.chart_bar__inner}>
-        <div
-          className={classes.chart_bar__fill}
-          style={{ height: barFillHeight }}
-        ></div>
-      </div>
+    <div className={classes.chart_bar___container}>
       <div className={classes.chart_bar__label}>{label}</div>
-      <div className={classes.chart_bar__label}>{value + "$"}</div>
+      <div className={classes.chart_bar}>
+        <div className={classes.chart_bar__inner}>
+          <div
+            className={classes.chart_bar__fill}
+            style={{ height: barFillHeight }}
+          ></div>
+        </div>
+      </div>
+      <div className={classes.chart_bar__label}>{"$" + value}</div>
     </div>
   );
 }
